2e6ff2ae7724bbc938b60a397cd97a91f4de51eb
aka-vm <vineetmahajan2000@gmail.com>
2023-07-07 17:51:30 +0530

    scripting

---
+++ startup-script.sh
@@ -0,0 +1,7 @@
+# Git Init
+git config --global user.name "Vineet Mahajan"
+git config --global user.email "vineetmahajan2000@gmail.com"
+
+# Kaggle Init
+pip install kaggle --upgrade
+cp -r .kaggle ~/
